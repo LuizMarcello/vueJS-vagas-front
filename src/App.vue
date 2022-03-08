b6a4b1dc6354c34804e06559141b71a9b318faf0
luizmarcello <luizmarcello.vmo@hotmail.com>
2022-03-08 09:55:12 -0300

    Rolândia:08.03.2022_09:55hs

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
+/* Parte visual do componente. Elementos html */
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Componente App</h1>
+    <button @click="desmontarComponente()">
+      Desmontar o componente Conteudo
+    </button>
+    <!-- Seletor de componentes do template -->
+    <!-- Aqui, o nome do seletor do componente
+         pode ser em "kebab case", padrão igual
+         das tags html -->
+    <topo-padrao />
+    <conteudo v-if="visibilidade"></conteudo>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+/* Existem o "nome do script do componente", o "nome do
+   componente" e o "nome do seletor do componente" */
 
+/* Para codificar e exportar o objeto de configuração do vueJS,
+   para o componente */
+<script>
+/* Importando os componentes */
+/* Também em "Pascal case" */
+/* O alias "@" indica sempre o "src" do projeto, para importar */
+import Conteudo from "@/components/layouts/Conteudo.vue";
+import TopoPadrao from "@/components/layouts/TopoPadrao.vue";
+
+/* Definindo estes componentes no objeto de configuração
+   da instância do vueJS, na propriedade "componentes" */
 export default {
-  name: 'App',
+  name: "App",
+
+  /* Propriedades reativas */
+  data: () => ({
+    visibilidade: true,
+  }),
+
   components: {
-    HelloWorld
-  }
-}
+    /* Seletor e componente */
+    /* Aqui, o nome do seletor pode ser definido
+       em "PascalCase" */
+    Conteudo,
+    TopoPadrao,
+  },
+
+  methods: {
+    desmontarComponente() {
+      this.visibilidade = false;
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Define o estilo */
+/* Na hierarquia, o estilo visual definido no componente de nível
+   mais alto, é propagado para os componentes inferiores */
+<style scoped>
 </style>
